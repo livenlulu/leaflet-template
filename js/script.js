240c7800b2b9866b92c127d411cdfba4c7beb128
Lucy Kang <lucykang@nyu.edu>
2016-12-07 23:13:35 -0800

    first

---
+++ js/script.js
@@ -0,0 +1,240 @@
+var layer = L.tileLayer('https://api.mapbox.com/styles/v1/livenlulu/ciu0azvas00322in5xzze3u48/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGl2ZW5sdWx1IiwiYSI6ImNpZ3h0ZzltbzB1cTQ0cG0zamthcno1dmwifQ.vZrmbXCCq15ZVuF6g6vhkA',{
+    attribution: ''
+});
+
+var map = L.map('myMap', { 
+  attributionControl: false,
+  tap:false
+}).setView( [40.738153,-73.913612], 12);
+map.addLayer(layer);
+
+var rentData = [];
+rentData[0]={};
+var currid=0;
+var med=0;
+    
+
+var chart;
+
+var manhattan = [40.763121,-73.948288];
+var brooklyn = [40.637925,-73.948288];
+var bronx = [40.841606, -73.874817];
+var queens = [40.716298,-73.853874];
+var statenisland = [40.571719,-74.148788];
+
+var panOptions = {
+    animate: true,
+    duration: 2
+ 	}
+
+      $(".myButton").click(function() {
+      if($(this).attr('id') == 'one' ) {
+        map.panTo(manhattan, panOptions);
+      } 
+      
+      else if 
+      ($(this).attr('id') == 'two' ) {
+        $(this).css('background-color','#fff');
+        map.panTo(brooklyn, panOptions);
+      } 
+
+      else if 
+      ($(this).attr('id') == 'three' ) {
+        $(this).css('background-color','#fff');
+        map.panTo(bronx, panOptions);
+      } 
+
+      else if 
+      ($(this).attr('id') == 'four' ) {
+        $(this).css('background-color','#fff');
+        map.panTo(queens, panOptions);
+      } 
+
+      else {
+        $(this).css('background-color','#fff');
+        map.panTo(statenisland, panOptions);
+      }
+    });
+      
+// function onEachFeature(feature, layer) {
+//     // does this feature have a property named popupContent?
+//     if (feature.properties && feature.properties.popupContent) {
+//         layer.bindPopup(feature.properties.popupContent);
+//     }
+// }
+
+  $("#about-btn").click(function() {
+  $("#aboutModal").modal("show");
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
+    });
+
+  var geojson;
+
+  $.getJSON('data/query.geojson', function(data) {
+    geojson = L.geoJson(data, {
+    	style: style,
+    	onEachFeature: onEachFeature
+    }).addTo(map);
+    updateChart(data.features[currid].properties)
+  });
+
+
+  function getColor(d) {
+    return d > 90 ? '#900000' :
+           d > 80  ? '#BD0026' :
+           d > 60  ? '#E31A1C' :
+           d > 50  ? '#FC4E2A' :
+           d > 40  ? '#FD8D3C' :
+           d > 30  ? '#FEB24C' :
+           d > 20  ? '#FED976' :
+                     '#FFEDA0' ;
+  }
+
+
+  function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.shape_star),
+        weight: .2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '0',
+        fillOpacity: 0.7
+    };
+  }
+
+  function mouseoverFunction(e) {
+    var layer = e.target;
+    // med value
+    //med = e.target.feature.properties.median_income;
+    //console.log(med);
+
+    layer.setStyle({
+        weight: 5,
+        opacity: 1,
+        color: 'white',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+    // try updatechart
+    updateChart(e.target.feature.properties);
+
+    // console.log(layer.feature.properties.VALUE2);
+    $('#side').html('<h4><b><font color="#000">' + layer.feature.properties.VALUE2 + '%' +'</font></b>...' + '</h4>');
+  	}
+
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+  }
+
+  function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: mouseoverFunction,
+        mouseout: resetHighlight
+        //click: zoomToFeature
+    });
+  }
+
+// subway stations
+// $.getJSON('data/subwaystop.geojson', function(data2) {
+//   // console.log(data);
+
+// var subwaystations = {
+//     radius: 2,
+//     fillColor: "green",
+//     color: "#fff",
+//     weight: .5,
+//     opacity: 1,
+//     fillOpacity: 01,
+    
+// };
+
+// L.geoJson(data2, {
+//     pointToLayer: function (feature, latlng) {
+//         return L.circleMarker(latlng, subwaystations);
+//     }
+// }).addTo(map);
+// });
+
+//dropdown scroll
+  $(".dropdown-menu li a").click(function(){
+  var selText = $(this).text();
+  $(this).parents('.dropdown').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+  });
+
+
+//bar chart
+nv.addGraph(function() {
+  chart = nv.models.discreteBarChart()
+    .x(function(d) { return d.label })
+    .y(function(d) { return d.value })
+    .staggerLabels(true)
+    .showValues(true)
+    .margin({left:0,right:0})
+    .color(['rgb(77,175,74)','rgb(55,126,184)','rgb(228,26,28)'])
+    .valueFormat(function(d){
+        return Math.round(d * 10)/10;
+      });
+    ;
+
+  nv.utils.windowResize(chart.update);
+
+  return chart;
+});
+
+
+//Each bar represents a single discrete quantity.
+function updateChart(f){
+
+  rentData[0].key = "vacancyrent";
+  rentData[0].values =
+    [
+        { 
+          "label" : "Median Monthly Income" , 
+          "value" : f.median_income / 12
+        } , 
+        { 
+          "label" : "Median Monthly Rent" , 
+          "value" : f.median_rent 
+        } , 
+        { 
+          "label" : "30% Of A Household's Total Income" , 
+          "value" : f.median_income /12 * .3
+        } 
+      ]
+    d3.select('#chart svg')
+    .datum(rentData)
+    .transition().duration(500)
+    .call(chart);
+  
+}
+
+
+//bulletchart
+// nv.addGraph(function() {  
+//   var chart2 = nv.models.bulletChart();
+
+//   d3.select('#chart2 svg')
+//       .datum(exampleData())
+//       .transition().duration(1000)
+//       .call(chart2);
+
+//   return chart2;
+// });
+
+
+// function exampleData() {
+//   return {
+//     "title":"Revenue",    //Label the bullet chart
+//     "subtitle":"US$",   //sub-label for bullet chart
+//     "ranges":[150,225,300],  //Minimum, mean and maximum values.
+//     "measures":[220],    //Value representing current measurement (the thick blue line in the example)
+//     "markers":[250]      //Place a marker on the chart (the white triangle marker)
+//   };
+// }
+
+
